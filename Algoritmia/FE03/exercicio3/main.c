d80a61290909ba329a54a5f2b09fcdc68c213e1b
Roberto Barreto <a21123@alunos.ipca.pt>
2020-11-20 16:37:02 +0000

    ex3 algoritmos

--- Algoritmia/FE03/exercicio3/main.c
+++ Algoritmia/FE03/exercicio3/main.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
+#include<string.h>
 
 int main(int argc, char const *argv[])
 {
     int count=0;
     float tempC,tempK,tempK_1=0,MtempK,mTempK,media,totalTempsK;
     char escolha;
+    char temp[100];
+    char rel[500];
     while(1){
         printf("Introduza uma temperatura em celcius: ");scanf("%f",&tempK);
         tempC=tempK-273;
@@ -20,6 +23,8 @@ int main(int argc, char const *argv[])
         
         printf("tempK_1: %.2f",tempK_1);
         printf("\nTemperatura: %.2f Kelvin -> %.2f Celcius",tempK,tempC);
+        sprintf(temp,"Temperatura: %.2f Kelvin -> %.2f Celcius",tempK,tempC);
+        strcat(rel,temp);
         count++;
 
         totalTempsK+=tempK;
@@ -30,12 +35,15 @@ int main(int argc, char const *argv[])
 
         tempK_1=tempK;
     }
-
     media=totalTempsK/count;
-
-    printf("\nMaior temperatura em kelvin foi: %.2f",MtempK);
-    printf("\nMenor temperatura em kelvin foi: %.2f",mTempK);
-    printf("\nMedia de temperaturas em kelvin e: %.2f",media);
+    printf("**************************************************\n");
+    printf("**\t\tRelatorio         \t\t**\n");
+    printf("** %s **",rel);
+    printf("\n**************************************************");
+    printf("\n** Maior temperatura em kelvin foi: %.2f \t**",MtempK);
+    printf("\n** Menor temperatura em kelvin foi: %.2f \t**",mTempK);
+    printf("\n** Media de temperaturas em kelvin e: %.2f \t**",media);
+    printf("\n**************************************************");
 
     return 0;
 }
