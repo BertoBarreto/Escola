37202974d4e376cbb267e20a7ad31ec379595182
Roberto Barreto <a21123@alunos.ipca.pt>
2020-11-20 17:12:56 +0000

    Exercicio 9 finalizado

--- Algoritmia/Trabalho/trabalho2/exercicio9/main.c
+++ Algoritmia/Trabalho/trabalho2/exercicio9/main.c
@@ -4,22 +4,24 @@
 int main(){
    char p[50];
    char p_[50];
-   int verifica,n1,n2,prod;
+   int verifica,n1,n2,prod,melhorCap=0,i=0;
 
    printf("introduza os nums(n1/n2):");scanf("%d %d",&n1,&n2);
    prod=n1*n2;
-   sprintf(p,"%d",prod);
-   sprintf(p_,"%d",prod);
-   
-   strcpy(p_,p);
-   strcpy(p_,strrev(p));
+   for(i=1;i<=prod+1;i++){
+      sprintf(p,"%d",i);
+      sprintf(p_,"%d",i);
 
-   verifica=strcmp(p,p_);
-   printf("ver: %d\n",verifica);
-   if(verifica==0)
-      printf("Nao e capicua");
-   else
-      printf("capicua");
+      strrev(p_);
+
+      verifica=strcmp(p,p_);
+     
+      if(verifica==0)
+         melhorCap=i; 
+
+   }
+
+   printf("Maior capicua:%d",melhorCap);
    // puts(strrev(p));
 
    return 0;
