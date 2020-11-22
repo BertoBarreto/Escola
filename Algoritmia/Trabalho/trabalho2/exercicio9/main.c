bd1a0ea7288334a9a0130b5d99995156ac9d008c
Roberto Barreto <a21123@alunos.ipca.pt>
2020-11-22 18:37:08 +0000

    Arranjo ex 9 e resolução dos ex da ficah 3

--- Algoritmia/Trabalho/trabalho2/exercicio9/main.c
+++ Algoritmia/Trabalho/trabalho2/exercicio9/main.c
@@ -13,24 +13,26 @@ int main(){
    int verifica,n1,n2,prod,melhorCap=0,i=0;
 
    printf("introduza os nums(n1/n2):");scanf("%d %d",&n1,&n2);
-   prod=n1*n2;
-   //verficar todos os numeros até ao resultado do produto
-   for(i=1;i<=prod;i++){
-      //guardar o valor dos numeros em dois vetores
-      sprintf(p,"%d",i);
-      sprintf(p_,"%d",i);
-      //inverter um vetor
-      strrev(p_);
-      //comparar se os dois vetores são iguais
-      verifica=strcmp(p,p_);
-     //caso sejam iguais a maior capicua e este valor
-      if(verifica==0)
-         melhorCap=i; 
+   if((n1>=100)&&(n2>=100)){
+      prod=n1*n2;
+      //verficar todos os numeros até ao resultado do produto
+      for(i=1;i<=prod;i++){
+         //guardar o valor dos numeros em dois vetores
+         sprintf(p,"%d",i);
+         sprintf(p_,"%d",i);
+         //inverter um vetor
+         strrev(p_);
+         //comparar se os dois vetores são iguais
+         verifica=strcmp(p,p_);
+      //caso sejam iguais a maior capicua e este valor
+         if(verifica==0)
+            melhorCap=i; 
+      }
 
+      printf("Maior capicua:%d",melhorCap);
+   }else{
+      printf("Erro");
    }
-
-   printf("Maior capicua:%d",melhorCap);
-   
 
    return 0;
 }
