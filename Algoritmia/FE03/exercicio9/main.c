a20ff8f17cf926a49ededcbfa06431c02ced9b11
Roberto Barreto <a21123@alunos.ipca.pt>
2020-11-24 17:11:59 +0000

    exercicio9

---
+++ Algoritmia/FE03/exercicio9/main.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+void main(){
+    int altura;
+    char carater;
+
+    printf("Introduza a altura do triangulo: ");scanf("%d",&altura);
+    printf("Introduza o carater para preencher: ");scanf(" %c",&carater);
+
+    for(int i=1;i<=altura;i++){
+
+        for(int a=1;a<=i;a++){
+            printf(" %c",carater);
+        }
+        printf("\n");
+
+    }
+
+}
