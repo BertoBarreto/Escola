a4acb9993788467162abfb4054f07f314f3d6ae0
Roberto Barreto <a21123@alunos.ipca.pt>
2020-11-25 13:41:24 +0000

    FE03-Exercicio 1 algoritmo e arranjo de codigo

--- Algoritmia/FE03/exercicio1/main.c
+++ Algoritmia/FE03/exercicio1/main.c
@@ -1,3 +1,9 @@
+/*********************************
+**    Nome: Peso de pessoas     **
+**    Autor: Roberto Barreto    **
+**    Data: 21/11/2020          **
+**********************************/
+
 #include<stdio.h>
 
 int main(int argc, char const *argv[])
@@ -5,7 +11,6 @@ int main(int argc, char const *argv[])
     int peso,contador=0,contadorP=0;
     char decisao;
     float media = 0.0f;
-    int max;
 
     while(1){
         printf("Introduza o peso da pessoa:");scanf("%d",&peso);
@@ -23,8 +28,6 @@ int main(int argc, char const *argv[])
     
     }    
     media=(float)contadorP/(float)contador;
-    printf("contador: %d\n",contador);
-    printf("contadorP: %d\n",contadorP);
     printf("Num total de %d pessoas, a media de pessoas com peso entre 50 e 80 kg e: %.2f",contador,media);
     return 0;
 }
--- Algoritmia/FE04/exercicio1/main.c
+++ Algoritmia/FE04/exercicio1/main.c
@@ -1,3 +1,9 @@
+/*********************************
+**    Nome: Numero 33           **
+**    Autor: Roberto Barreto    **
+**    Data: 27/10/2020          **
+**********************************/
+
 #include<stdio.h>
 
 void main(){
