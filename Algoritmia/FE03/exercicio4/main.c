1e22a748e7af5d3dd27043b496d8f0ee6f900006
Roberto Barreto <a21123@alunos.ipca.pt>
2020-11-25 12:32:44 +0000

    correção ex 3 e 4

--- Algoritmia/FE03/exercicio4/main.c
+++ Algoritmia/FE03/exercicio4/main.c
@@ -5,8 +5,8 @@ void main(){
     int num,resultado=1,i;
 
     printf("Introduza o seu numero: ");scanf("%d",&num);
-    printf("Fatorial de %d: 1 x",num);
-    for(i=2;i<=num;i++){
+    printf("Fatorial de %d: ",num);
+    for(i=1;i<=num;i++){
         resultado*=i;
         if(i==num)
             printf(" %d",i);
