69eaf9b27906623e9aa6827e8c5554738422d252
Roberto Barreto <a21123@alunos.ipca.pt>
2020-11-28 10:57:42 +0000

    FE04- Exercicio 3 codigo

--- Algoritmia/FE04/exercicio1/main.c
+++ Algoritmia/FE04/exercicio1/main.c
@@ -6,8 +6,18 @@
 
 #include<stdio.h>
 
+int soma(int n1,int n2);
+
+int soma(int n1, int n2){
+    return n1+n2;
+}
+
 void main(){
 
-    
+    int n1,n2;
+    printf("Introduza dois numeros: ");scanf("%d %d",&n1,&n2);
+
+    printf("%d + %d = %d",n1,n2,soma(n1,n2));
+
 
 }
