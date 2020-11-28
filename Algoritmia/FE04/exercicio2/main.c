d2213db8c79a2acba862f07e92b3702dbe8385c6
Roberto Barreto <a21123@alunos.ipca.pt>
2020-11-28 11:06:21 +0000

    FE04-exercicio2 codigo

---
+++ Algoritmia/FE04/exercicio2/main.c
@@ -0,0 +1,22 @@
+/*********************************
+**    Nome: Soma                **
+**    Autor: Roberto Barreto    **
+**    Data: 28/11/2020          **
+**********************************/
+
+#include<stdio.h>
+
+int media(int n1,int n2,int n3);
+
+int media(int n1,int n2,int  n3){
+    return (n1+n2+n3)/3;
+}
+
+void main(){
+
+    int n1,n2,n3;
+
+    printf("Introduza os 3 numeros: ");scanf("%d %d %d",&n1,&n2,&n3);
+
+    printf("A media de %d, %d, %d e: %d",n1,n2,n3,media(n1,n2,n3));
+}
