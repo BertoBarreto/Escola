81efe3203f280110c4f0eb812b7a56dd1bd9d8c5
Roberto Barreto <a21123@alunos.ipca.pt>
2020-12-01 13:25:41 +0000

    FE04- Ex8 codigo

---
+++ Algoritmia/FE04/exercicio8/main.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+void verificaPrimo(int numero);
+
+void verificaPrimo(int numero){
+    int primo=1;
+    //todos os numeros sao divisiveis por 1 e por ele próprio logo temos de testar
+    //se num é divisivel por qualquer numero entre 2 e o num-1
+    for (int a = 2; a <= numero-1; a++){ 
+    //se o resto for 0, ou seja existe um numero divisor de num no intervalo entre 2 e num-1
+        if (numero % a == 0) {
+            primo=0;
+            printf("\nO numero %d nao e primo",numero);
+            break;
+        }
+    }
+    //partimos sempre do principio q o numero é primo, assim só quando se verifica que não é primo é que nao incrementamos
+    if(primo==1){
+        printf("\nO numero %d e primo",numero);
+    }
+}
+
+void main(){
+    int num1,num2; 
+
+    printf("Introduza o intervalo: ");scanf("%d;%d",&num1,&num2);
+
+    for(int i=num1;i<=num2;i++)
+        verificaPrimo(i);   
+    
+}
