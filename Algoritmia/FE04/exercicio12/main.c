9b8dbb4074f47c9bfdb5e0c78b0c8a57676a33f9
Roberto Barreto <a21123@alunos.ipca.pt>
2020-12-04 16:03:47 +0000

    FE04- ex11 e 12

---
+++ Algoritmia/FE04/exercicio12/main.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int potencia(int num, int expoente);
+
+int potencia(int num, int expoente){
+    if(expoente == 0)
+        return 0;
+    else
+        return (num*potencia(num,expoente-1));
+    
+}
+
+void main(){
+
+    int num, exp;
+
+    printf("Introduza o numero e o expoente");scanf("%d %d",&num,&exp);
+    printf("Resultado da potencia e: %d",potencia(num,exp));
+
+
+}
