dc986e922f4727fc5a47977e0dbf10decd48594e
Roberto Barreto <a21123@alunos.ipca.pt>
2020-12-04 14:58:13 +0000

    FE04- Ex10 codigo

---
+++ Algoritmia/FE04/exercicio10/main.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int calculaFatorial(int n);
+
+int calculaFatorial(int n){
+    int resultado=1;
+    for(int i=1;i<=n;i++){
+        resultado*=i;
+    }
+    return resultado;
+}
+
+
+void main(){
+
+    int n;
+
+    printf("Introduza o seu numero: ");scanf("%d",&n);
+
+    printf("O resultado do fatorial de %d e: %d",n,calculaFatorial(n));
+
+}
