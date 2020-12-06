519365ee03d69dbc9617e50a806a60fea6757551
Roberto Barreto <a21123@alunos.ipca.pt>
2020-12-06 13:00:31 +0000

    FE04-Ex3 algoritmo

--- Algoritmia/FE04/exercicio3/main.c
+++ Algoritmia/FE04/exercicio3/main.c
@@ -4,37 +4,39 @@
 **    Data: 28/11/2020          **
 **********************************/
 
-#include<stdio.h>
-#include<ctype.h>
+#include <stdio.h>
+#include <ctype.h>
 
-
-int soma(int n1,int n2);
+int soma(int n1, int n2);
 float divisor(float dividendo, float divisor);
 
-int soma(int n1, int n2){
-    return n1+n2;
+int soma(int n1, int n2)
+{
+    return n1 + n2;
 }
 
-float divisor(float dividendo, float divisor){
-    return dividendo/divisor;
+float divisor(float dividendo, float divisor)
+{
+    return dividendo / divisor;
 }
 
-void main(){
+void main()
+{
 
-    int idade,contador=0,total;
+    int idade, contador = 0, total = 0;
     char escolha;
 
-    do{
-        printf("Idade?");scanf("%d",&idade);//14//15//18
-        if(contador==0)
-            total=idade;//total=14
-        else
-            total=soma(idade,total);//total=soma(15,14)//total=(18,29)
-            
-        printf("Deseja sair?(S/N)");scanf(" %c",&escolha);
-        contador++;
-    }while(toupper(escolha)=='N');
-    
-    printf("A media e: %.2f",divisor((float)total,(float)contador));
+    do
+    {
+        printf("Idade?");
+        scanf("%d", &idade); //14//15//18
 
+        total = soma(idade, total); //total=soma(15,14)//total=(18,29)
+
+        printf("Deseja sair?(S/N)");
+        scanf(" %c", &escolha);
+        contador++;
+    } while (toupper(escolha) == 'N');
+
+    printf("A media e: %.2f", divisor((float)total, (float)contador));
 }
