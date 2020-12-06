49592687e746e568c90040e33b251df346439580
Roberto Barreto <a21123@alunos.ipca.pt>
2020-12-06 13:53:05 +0000

    FE04-Ex7 algoritmo

--- Algoritmia/FE04/exercicio7/main.c
+++ Algoritmia/FE04/exercicio7/main.c
@@ -1,30 +1,40 @@
-#include<stdio.h>
+/*********************************
+**    Nome: verificaPrimo       **
+**    Autor: Roberto Barreto    **
+**    Data: 28/11/2020          **
+**********************************/
+#include <stdio.h>
 
 void verificaPrimo(int numero);
 
-void verificaPrimo(int numero){
-    int primo=1;
+void verificaPrimo(int numero)
+{
+    int primo = 1;
     //todos os numeros sao divisiveis por 1 e por ele próprio logo temos de testar
     //se num é divisivel por qualquer numero entre 2 e o num-1
-    for (int a = 2; a < numero; a++){ 
-    //se o resto for 0, ou seja existe um numero divisor de num no intervalo entre 2 e num-1
-        if (numero % a == 0) {
-            primo=0;
+    for (int a = 2; a < numero; a++)
+    {
+        //se o resto for 0, ou seja existe um numero divisor de num no intervalo entre 2 e num-1
+        if (numero % a == 0)
+        {
+            primo = 0;
             printf("O numero nao e primo");
             break;
         }
     }
     //partimos sempre do principio q o numero é primo, assim só quando se verifica que não é primo é que nao incrementamos
-    if(primo==1){
+    if (primo == 1)
+    {
         printf("O numero e primo");
     }
 }
 
-void main(){
-    int num; 
+void main()
+{
+    int num;
 
-    printf("Introduza o numero: ");scanf("%d",&num);
+    printf("Introduza o numero: ");
+    scanf("%d", &num);
 
-    verificaPrimo(num);   
-    
+    verificaPrimo(num);
 }
